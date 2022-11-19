81699ea8ab847db4571b54497a0f5225caf714d4
tanjim30 <118615452+tanjim30@users.noreply.github.com>
2022-11-19 22:13:27 +0600

    Add files via upload

---
+++ 1202 module3 topic2 BinarySearch.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+
+class BinarySearch
+{
+private:
+    int Beg,End,Mid,flag=0;
+
+public:
+    void Search(int ax[], int n, int x)
+    {
+        Beg=0;
+        End = n-1;
+
+        while(Beg <= End)
+        {
+            Mid =Beg + (End-Beg)/2;
+            if(ax[Mid] == x)
+            {
+                cout << Mid+1;
+                flag=1;
+                break;
+            }
+            if(ax[Mid] > x)
+            {
+                End = Mid - 1;
+            }
+            if(ax[Mid] < x)
+            {
+              Beg = Mid + 1;
+            }
+        }
+        if(flag==0)
+        {
+            cout << "Not found!!";
+        }
+    }
+};
+
+int main()
+{
+    int n,i,x;
+
+    cout << "Enter how many elements" << endl;
+    cin >> n;
+    int ax[n];
+    cout << "Enter array values in a shorted manner" << endl;
+    for(i=0;i<n;i++)
+    {
+        cin >> ax[i];
+    }
+    cout << "Enter value for search" << endl;
+    cin >> x;
+
+    BinarySearch t;
+    t.Search(ax,n,x);
+    return 0;
+}
+
